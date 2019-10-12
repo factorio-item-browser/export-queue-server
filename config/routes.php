a93a87f5d756d403b7b56f0203863b00972b8b1d
BluePsyduck <bluepsyduck@gmx.com>
2019-10-12 17:26:02 +0200

    Disallowed invalid values as UUID in routes.

--- config/routes.php
+++ config/routes.php
@@ -17,8 +17,10 @@ use Zend\Expressive\Application;
 use Zend\Expressive\MiddlewareFactory;
 
 return function (Application $app, MiddlewareFactory $factory, ContainerInterface $container): void {
+    $regexUuid = '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}';
+
     $app->post('/job', Handler\Job\AddHandler::class, RouteName::JOB_ADD);
-    $app->get('/job/{job-id:[0-9a-f-]+}', Handler\Job\GetHandler::class, RouteName::JOB_GET);
-    $app->patch('/job/{job-id:[0-9a-f-]+}', Handler\Job\UpdateHandler::class, RouteName::JOB_UPDATE);
+    $app->get(sprintf('/job/{job-id:%s}', $regexUuid), Handler\Job\GetHandler::class, RouteName::JOB_GET);
+    $app->patch(sprintf('/job/{job-id:%s}', $regexUuid), Handler\Job\UpdateHandler::class, RouteName::JOB_UPDATE);
     $app->get('/job/list', Handler\Job\ListHandler::class, RouteName::JOB_LIST);
 };
