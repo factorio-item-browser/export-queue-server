9f64fdaa9947d2edab81f321055ae52b661fc7a1
BluePsyduck <bluepsyduck@gmx.com>
2020-11-09 21:58:40 +0100

    Some preparations for new dev-box.

--- config/autoload/development/doctrine.local.php
+++ config/autoload/development/doctrine.local.php
@@ -11,36 +11,25 @@ declare(strict_types=1);
 
 namespace FactorioItemBrowser\ExportQueue\Server;
 
-use Doctrine\DBAL\Driver\PDOMySql\Driver as PDOMySqlDriver;
+use Doctrine\DBAL\Driver\PDO\MySQL\Driver as PDOMySqlDriver;
 use PDO;
 
 return [
     'doctrine' => [
-//        'configuration' => [
-//            'orm_default' => [
-//                'metadata_cache' => 'filesystem',
-//                'query_cache' => 'filesystem',
-//            ]
-//        ],
         'connection' => [
             'orm_default' => [
                 'driverClass' => PDOMySqlDriver::class,
                 'params' => [
-                    'host'     => 'fib-eq-mysql',
+                    'host'     => 'fib-mysql',
                     'port'     => '3306',
-                    'user'     => 'docker',
-                    'password' => 'docker',
-                    'dbname'   => 'docker',
+                    'user'     => 'export-queue',
+                    'password' => 'export-queue',
+                    'dbname'   => 'export-queue',
                     'driverOptions' => [
-                        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-                    ]
-                ]
-            ]
+                        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+                    ],
+                ],
+            ],
         ],
-//        'driver' => [
-//            'orm_default' => [
-//                'cache' => 'filesystem',
-//            ]
-//        ],
     ],
 ];
